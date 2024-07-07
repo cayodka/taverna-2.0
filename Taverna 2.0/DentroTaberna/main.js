e4fb322c0a61c151dac80c0cf14dd222cf17b7e5
Cayo デカア <144740711+cayodka@users.noreply.github.com>
2024-07-07 11:45:32 -0300

    Add files via upload

--- Taverna 2.0/DentroTaberna/main.js
+++ Taverna 2.0/DentroTaberna/main.js
@@ -1,9 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const som2 = new Audio('../sound/sound.mp3');
+
+        document.getElementById('body').addEventListener('mouseover', () => {
+            som2.play();
+            som1.onload = 'auto';
+    });
+});
+
+
+
+
 const som1 = new Audio('../sound/fire.mp3');
 
 document.getElementById('armor').addEventListener('click', () => {
     som1.play();
     som1.preload = 'auto';
 });
+let contador1 = 0;
+armor.addEventListener('click', () => {
+    contador1++;
+    
+    if (contador1 === 1) {
+        window.alert(`Você recebeu ${contador1} espada.`);
+    } else {
+        window.alert(`Você recebeu ${contador1} espada.`);
+    }
+});
+
+let contador2 = 0;
+potion.addEventListener('click', () => {
+    contador2++;
+    
+    if (contador2 === 1) {
+        window.alert(`Você recebeu ${contador2} poção.`);
+    } else {
+        window.alert(`Você recebeu ${contador2} poção.`);
+    }
+});
+
 
 const som2 = new Audio('../sound/hello.mp3');
 
--- Taverna 2.0/main.js
+++ Taverna 2.0/main.js
@@ -1,15 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const som2 = new Audio('./sound/sound.mp3');
+
+        document.getElementById('body').addEventListener('mouseover', () => {
+            som2.play();
+            som1.onload = 'auto';
+    });
+});
+
+// função da porta
+
 const som1 = new Audio('./sound/door.mp3');
 
 document.getElementById('porta').addEventListener('click', () => {
     som1.play();
     som1.preload = 'auto';
-});
-
-const som2 = new Audio('./sound/sounddark.mp3');
-
-document.getElementById('letreiro').addEventListener('click', () => {
-    som2.play();
-    som2.preload = 'auto';
 });
 
 document.getElementById('porta').addEventListener('click', () => {
